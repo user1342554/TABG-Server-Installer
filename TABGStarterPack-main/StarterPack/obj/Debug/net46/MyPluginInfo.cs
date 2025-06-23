1c904b1587f6374cdf3b325f2a11d4951c5cfe60
Jonas <Jonaspaul-neubauer@.de>
2025-06-23 16:16:07 +0200

    Remove build artifacts and binaries, add .gitignore

--- TABGStarterPack-main/StarterPack/obj/Debug/net46/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace StarterPack
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "StarterPack";
-        public const string PLUGIN_NAME = "StarterPack";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
