9a1b1e77f476742be89da69361f668b5c1ce4b33
Jonas <user1342554@users.noreply.github.com>
2026-04-08 17:05:08 +0200

    chore: add xUnit test project skeleton with smoke test

---
+++ TabgInstaller.Tests/SmokeTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace TabgInstaller.Tests
+{
+    public class SmokeTest
+    {
+        [Fact]
+        public void TestProjectCompiles()
+        {
+            Assert.True(true);
+        }
+    }
+}
